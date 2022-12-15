383ae058c49877108fd9b77d4beb92b5cc561718
Bohdana Omelchuk <dytynchukbogdana@gmail.com>
2022-12-15 13:29:16 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type headerProps = {
+    name: string;
 }
+
+const Header: React.FC<headerProps> = (props) => (
+    <header>Header {props.name}</header>
+)
+
+const App = () => (
+    <div className="App">
+        <Header name='WT'/>
+        <h1>Project</h1>
+    </div>
+)
 
 export default App;
